209e2d8dc209623fc8f5a60a29bbb93fe2a8cd8a
Corson Androski <101475962+corsonandroski@users.noreply.github.com>
2023-10-02 16:02:54 -0500

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,4 +1,4 @@
-const article = document.querySelector("article");
+//const article = document.querySelector("article");
 console.log('extension running');
 
 function httpGet(theUrl)
@@ -9,4 +9,27 @@ function httpGet(theUrl)
     return xmlHttp.responseText;
 }
 
-httpGet("https://era.snapschedule365.com/dataapi/ERA/CalendarView?monthDate=2023-10-01");
+function getMonthFromString(mon){
+   return new Date(Date.parse(mon +" 1, 2012")).getMonth()+1
+}
+
+const month = getMonthFromString(document.getElementById("txtMonthPicker").value.split(" ")[0]);
+const year = document.getElementById("txtMonthPicker").value.split(" ")[1];
+
+const response = httpGet(`https://era.snapschedule365.com/dataapi/ERA/CalendarView?monthDate=${year}-${month}-01`);
+
+//Get employee ID to filter assigned shifts
+const employeeID = response.MyEmployee.ID
+
+//Get shift definitions, create object with shift ID keys and shift description values
+const shifts = Object.fromEntries(response.Shifts.map((s) => [parseInt(s.ID), s.Description]))
+
+//Create CSV starting with headers
+let gCal = `Subject, Start Date, Start Time, End Time`
+  .concat(response.ShiftAssignments
+      .filter((s) => s.EmployeeID == employeeID)
+      .map((s) =>
+           `\n${response.Shifts.find(x => x.ID == s.ShiftID).Description}, ${new Date(s.StartDateTime).toLocaleDateString()}, ${new Date(s.StartDateTime).toLocaleTimeString()}, ${new Date(s.EndDateTime.split("+")[0]).toLocaleTimeString()}`)
+   ).toString()
+
+console.log(gCal)
