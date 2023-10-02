32ba578791c81dd2f48ea4f99f040c32cad3aefd
Corson Androski <101475962+corsonandroski@users.noreply.github.com>
2023-10-02 17:33:55 -0500

    Update content.js

--- scripts/content.js
+++ scripts/content.js
@@ -1,63 +1,65 @@
 //const article = document.querySelector("article");
 console.log('extension running');
 
-// Select the node that will be observed for mutations
-const targetNode = document.getElementById("txtMonthPicker");
-
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true };
-
-// Callback function to execute when mutations are observed
-const callback = (mutationList, observer) => {
-  for (const mutation of mutationList) {
-    if (mutation.type === "childList") {
-      console.log("A child node has been added or removed.");
-    } else if (mutation.type === "attributes") {
-      console.log(`The ${mutation.attributeName} attribute was modified.`);
-    }
-  }
-};
-
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
-
-// Later, you can stop observing
-//observer.disconnect();
-
-function httpGet(theUrl)
+function httpGet(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
 }
 
 function getMonthFromString(mon){
    return new Date(Date.parse(mon +" 1, 2012")).getMonth()+1
 }
 
-const month = getMonthFromString(document.getElementById("txtMonthPicker").value.split(" ")[0]);
-const year = document.getElementById("txtMonthPicker").value.split(" ")[1];
 
-console.log(month, year);
+let interval;
 
-const response = httpGet(`https://era.snapschedule365.com/dataapi/ERA/CalendarView?monthDate=${year}-${month}-01`);
+let viewedMonth;
 
-//Get employee ID to filter assigned shifts
-const employeeID = response.MyEmployee.ID
+interval = setInterval(getSchedule, 1000);
 
-//Get shift definitions, create object with shift ID keys and shift description values
-const shifts = Object.fromEntries(response.Shifts.map((s) => [parseInt(s.ID), s.Description]))
+function getSchedule(){
 
-//Create CSV starting with headers
-let gCal = `Subject, Start Date, Start Time, End Time`
-  .concat(response.ShiftAssignments
-      .filter((s) => s.EmployeeID == employeeID)
-      .map((s) =>
-           `\n${response.Shifts.find(x => x.ID == s.ShiftID).Description}, ${new Date(s.StartDateTime).toLocaleDateString()}, ${new Date(s.StartDateTime).toLocaleTimeString()}, ${new Date(s.EndDateTime.split("+")[0]).toLocaleTimeString()}`)
-   ).toString()
+  const month = getMonthFromString(document.getElementById("txtMonthPicker").value.split(" ")[0]);
+  const year = document.getElementById("txtMonthPicker").value.split(" ")[1];
 
-console.log(gCal)
+
+  console.log(month, viewedMonth);
+
+  if (viewedMonth !== month){
+
+    viewedMonth = month;
+
+    httpGet(`https://era.snapschedule365.com/dataapi/ERA/CalendarView?monthDate=${year}-${month}-01`, processResponse);
+  }
+}
+
+function processResponse(r){
+
+  let response = JSON.parse(r);
+
+  console.log(response);
+
+  //Get employee ID to filter assigned shifts
+  const employeeID = response.MyEmployee.ID;
+
+  //Get shift definitions, create object with shift ID keys and shift description values
+  const shifts = Object.fromEntries(response.Shifts.map((s) => [parseInt(s.ID), s.Description]))
+
+  //Create CSV starting with headers
+  let gCal = `Subject, Start Date, Start Time, End Time`
+    .concat(response.ShiftAssignments
+        .filter((s) => s.EmployeeID == employeeID)
+        .map((s) =>
+             `\n${response.Shifts.find(x => x.ID == s.ShiftID).Description}, ${new Date(s.StartDateTime).toLocaleDateString()}, ${new Date(s.StartDateTime).toLocaleTimeString()}, ${new Date(s.EndDateTime.split("+")[0]).toLocaleTimeString()}`)
+     ).toString()
+
+  console.log(gCal)
+
+  interval = null;
+}
