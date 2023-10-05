f596c3448b2b09eadd9b88a1908acff6a1f26540
Corson Androski <101475962+corsonandroski@users.noreply.github.com>
2023-10-05 11:04:26 -0500

    refactor content.js

--- scripts/content.js
+++ scripts/content.js
@@ -21,22 +21,46 @@ let interval;
 
 let viewedMonth;
 
-interval = setInterval(getSchedule, 1000);
+interval = setInterval(updateButton, 1000);
 
-function getSchedule(){
-
+function updateButton(){
+  //get month and year from page header
   const month = getMonthFromString(document.getElementById("txtMonthPicker").value.split(" ")[0]);
   const year = document.getElementById("txtMonthPicker").value.split(" ")[1];
 
+  console.log("checking for month change,", month, viewedMonth);
 
-  console.log(month, viewedMonth);
-
+  //if the month displayed on page is different than the one referenced by download button (because page just loaded or switched to new month)
   if (viewedMonth !== month){
 
+    //update download target month to match month displayed on page
     viewedMonth = month;
 
-    httpGet(`https://era.snapschedule365.com/dataapi/ERA/CalendarView?monthDate=${year}-${month}-01`, processResponse);
+    // updateDownloadButton
+    if (!document.getElementById("downloadButton")){
+      let button = document.createElement("button");
+      button.id = "downloadButton"
+      document.querySelectorAll("[data-role='navbar']")[1].appendChild(button);
+    }
+
+    button = document.getElementById("downloadButton");
+    button.innerHTML = `download ${
+      document.getElementById("txtMonthPicker").value.split(" ").join(" ")
+    } calendar`
+    button.style.position = "absolute";
+    button.style.top = "8px";
+    button.style.right = "120px";
+    button.style.width = "140px";
+    button.onclick = download;
   }
+}
+
+function download(){
+  console.log("downloading")
+  //get month and year from page header
+  const month = getMonthFromString(document.getElementById("txtMonthPicker").value.split(" ")[0]);
+  const year = document.getElementById("txtMonthPicker").value.split(" ")[1];
+  httpGet(`https://era.snapschedule365.com/dataapi/ERA/CalendarView?monthDate=${year}-${month}-01`, processResponse);
 }
 
 function processResponse(r){
@@ -51,27 +75,6 @@ function processResponse(r){
   //Get shift definitions, create object with shift ID keys and shift description values
   //const shifts = Object.fromEntries(response.Shifts.map((s) => [parseInt(s.ID), s.Description]))
 
-  // updateDownloadButton
-  if (!document.getElementById("downloadButton")){
-    let button = document.createElement("button");
-    button.id = "downloadButton"
-    document.querySelectorAll("[data-role='navbar']")[1].appendChild(button);
-  }
-
-  button = document.getElementById("downloadButton");
-  button.innerHTML = `download ${
-    document.getElementById("txtMonthPicker").value.split(" ").join(" ")
-  } calendar`
-  button.style.position = "absolute";
-  button.style.top = "8px";
-  button.style.right = "120px";
-  button.style.width = "14  0px";
-  button.onclick = download;
-
-  interval = null;
-}
-
-function download(response){
   //Create CSV starting with headers
   let gCalCSV = `Subject, Start Date, Start Time, End Time`
     .concat(response.ShiftAssignments
@@ -81,4 +84,6 @@ function download(response){
      ).toString()
 
   console.log(gCalCSV)
+
+  interval = null;
 }
